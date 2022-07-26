b943e14f9a68ef31f0eb426715e6db9b42a1568e
Neha Shetti <103949984+Neha-Shetti@users.noreply.github.com>
2022-07-26 22:21:55 +0530

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,11 @@
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Designed By Khushi, Neha and Mohana.</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>

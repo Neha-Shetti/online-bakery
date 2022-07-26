1f7f30d52ad25e1a08c0c095feb4547853ab977e
Neha Shetti <103949984+Neha-Shetti@users.noreply.github.com>
2022-07-26 22:17:58 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> Developed By Khushi, Neha and Mohana.</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
